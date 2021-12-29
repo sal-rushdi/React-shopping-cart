d8eff7b239ab55d3f76ddc6456bac3d0254d6e5f
sal-rushdi <salah.alrushdi@gmail.com>
2021-12-29 08:10:03 +0100

    products comp

---
+++ Client/src/components/Products/Products.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../../css/Products/Products.css"
+function Products(props) {
+    return (
+        <div className="products-wrapper">
+            {props.products.map(product => (
+                <div className='product-item' key={product.id}>
+                    <img src={product.imageUrl} alt={product.title} />
+                    <div className='product-desc'>
+                        <p>{product.title}</p>
+                        <span>{product.prise}</span>
+                    </div>
+                    <button>Add to Card</button>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+
+export default Products
