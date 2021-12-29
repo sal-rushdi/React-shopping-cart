aae7498a9b1b7f03f04db00fb644f65ffe2a857a
sal-rushdi <salah.alrushdi@gmail.com>
2021-12-30 00:35:57 +0100

    filter-comp

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer/footer";
 // import { words } from "./words";
 import data from './data.json'
 import Products from "./components/Products/Products";
+import Filter from "./components/Filter/Filter";
 
 function App() {
   const [products, setProducts] = useState(data)
@@ -14,7 +15,7 @@ function App() {
       <main>
         <div className="wrapper">
           <Products products={products} />
-          <div className="filter-wrapper">Filter</div>
+          <Filter />
         </div>
       </main>
       <Footer />
