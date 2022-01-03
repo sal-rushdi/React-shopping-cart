1789ade14c7362a4fea037f98c41dd87d33f0fa3
sal-rushdi <salah.alrushdi@gmail.com>
2022-01-03 20:24:44 +0100

    checkout

--- Client/src/components/Cart/Cart.js
+++ Client/src/components/Cart/Cart.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../../css/Cart/Cart.css"
+import Checkout from '../CheckoutForm/Checkout'
  function Cart(props) {
+
+    const [showForm, setShowForm] = useState(false)
+    const [value, setValue] = useState("")
+
+    const submitOrder = (e) => {
+        e.preventDefault();
+
+        const order = {
+            name : value.name,
+            email: value.email 
+        }
+        console.log(order);
+    }
+
+    const handleChange = (e) => {
+        setValue( (prevState) => ({...prevState, [e.target.name]: e.target.value}))
+    }
+
     return (
         <div className='cart-wrapper'>
             <div className='cart-title'> {props.cartItems.length === 0 ? 'cart Empty' : <p>
@@ -24,6 +43,27 @@ import "../../css/Cart/Cart.css"
                 ))}
                 
             </div>
+            {
+                props.cartItems.length !== 0 &&
+                (
+                    <div className='cart-footer'>
+                        <div className='total'>Total : ${props.cartItems.reduce( (acc, p) => {
+                            return acc + p.price
+                        } , 0)} </div>
+                        <button onClick={() => setShowForm(true)}>select products </button>
+                    </div>
+                )
+            }
+
+            {/* Checkout Form */}
+
+            <Checkout
+            showForm={showForm}
+            value={value}
+            submitOrder={submitOrder}
+            setShowForm={setShowForm}
+            handleChange={handleChange}
+            />
             
         </div>
     )
